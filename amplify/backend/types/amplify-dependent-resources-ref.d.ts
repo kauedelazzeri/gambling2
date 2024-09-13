9a4c2eae4c74a007c0c6fcb848ef359f7bbdb608
unknown <kdelazzeri@weg.net>
2024-09-13 20:29:38 -0300

    Inicio da integração de autenticacao 4d

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "gambling295899e73": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
